d1a2682a914f01e947300abb86040a0446ce536b
geo318 <geo.lomidze@gmail.com>
2023-11-11 15:48:55 +0400

    chore: generate inset types

--- types/schema.ts
+++ types/schema.ts
@@ -1,11 +1,15 @@
 import { z } from 'zod'
-import { category, product, subCategory } from '/server'
+import { category, post, product, slider, subCategory } from '/server'
 import { createInsertSchema } from 'drizzle-zod'
 
 export const insertCategorySchema = createInsertSchema(category)
 export const insertSubCategorySchema = createInsertSchema(subCategory)
 export const insertProductSchema = createInsertSchema(product)
+export const insetPostSchema = createInsertSchema(post)
+export const insetSliderSchema = createInsertSchema(slider)
 
 export type Category = z.infer<typeof insertCategorySchema>
 export type SubCategory = z.infer<typeof insertSubCategorySchema>
 export type Product = z.infer<typeof insertProductSchema>
+export type Post = z.infer<typeof insetPostSchema>
+export type Slider = z.infer<typeof insetSliderSchema>
