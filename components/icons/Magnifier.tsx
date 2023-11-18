9c1d67301ab481e28fc7d44bc802b0c868029a4e
geo318 <geo.lomidze@gmail.com>
2023-11-19 01:00:26 +0400

    style: search filter touchups

--- components/icons/Magnifier.tsx
+++ components/icons/Magnifier.tsx
@@ -1,7 +1,7 @@
 export const Magnifier = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width='13'
-    height='13'
+    width='15'
+    height='15'
     viewBox='0 0 13 13'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
--- components/product/Search/Search.tsx
+++ components/product/Search/Search.tsx
@@ -7,12 +7,12 @@ export const Search = () => {
   const { handleSearch } = useSearch()
   return (
     <div className='relative mb-14 mt-10'>
-      <Magnifier className='absolute left-0 mt-1/2 translate-y-1/2' />
+      <Magnifier className='absolute left-0 top-1/2 -translate-y-1/2 pb-[2px]' />
       <input
         type='text'
         name='search'
         placeholder='Search'
-        className='border-0 border-b pl-5 w-52'
+        className='border-0 border-b border-[#BEBEBE] pl-5 py-2 w-52'
         onChange={handleSearch}
       />
     </div>
