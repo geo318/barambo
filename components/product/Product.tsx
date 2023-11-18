98095ece007528aa788cb5970821334fe34079b3
geo318 <geo.lomidze@gmail.com>
2023-11-19 00:56:11 +0400

    style: realign products page and make small touchups

--- components/product/Product.tsx
+++ components/product/Product.tsx
@@ -9,28 +9,30 @@ import { ProductModal, SearchParamsWrapper } from '/components'
 export const Product: React.FC<{
   product: TProduct
   locale: Locale
-}> = ({ product, locale }) => {
+  index: number
+}> = ({ product, locale, index }) => {
   return (
     <>
       <Link className={twMerge('cursor-pointer')} href={`?id=${product.id}`}>
-        <div className='relative w-full h-52'>
+        <div className='relative w-full h-52 bg-zinc-200 bg-opacity-50 rounded-3xl'>
           <Image
             src={getImage`${product.thumbnail}`}
             alt='banner'
             className='rounded-[1rem] w-full h-full object-contain'
             width={200}
             height={200}
+            priority={index < 5}
           />
         </div>
         <div className='flex flex-col gap-2 mt-5'>
           <div
-            className='text-lg text-secondary line-clamp-2 text-ellipsis leading-relaxed'
+            className='text-lg text-secondary line-clamp-3 text-ellipsis leading-relaxed'
             dangerouslySetInnerHTML={{
               __html: product[`desc_${getLangKey(locale)}`],
             }}
           />
           <div className='border-b my-2 border-[#bebebe]' />
-          <h4 className='text-lg font-medium'>
+          <h4 className='text-lg font-medium truncate'>
             {product[`title_${getLangKey(locale)}`]}
           </h4>
         </div>
--- components/product/ProductList/ProductList.tsx
+++ components/product/ProductList/ProductList.tsx
@@ -9,13 +9,13 @@ export const ProductList: React.FC<{
 }> = ({ locale }) => {
   const { products, lastFeedElementRef, isLoading } = useProductList()
   return (
-    <section className='grid grid-cols-1 gap-6'>
+    <section className='grid grid-cols-4 gap-6'>
       {isLoading ? (
         <Spinner />
       ) : (
         products?.map((p, i) => (
           <div key={i} ref={lastFeedElementRef}>
-            <Product product={p} locale={locale} />
+            <Product product={p} locale={locale} index={i} />
           </div>
         ))
       )}
