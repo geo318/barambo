59721e521dc0515822ae618aae68775d0c11d209
geo318 <geo.lomidze@gmail.com>
2023-11-20 02:36:02 +0400

    test: force dynamic posts

--- app/[lang]/blog/[slug]/layout.tsx
+++ app/[lang]/blog/[slug]/layout.tsx
@@ -4,7 +4,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
 }
 
-export async function generateStaticParams() {
-  const posts = await getPostsSlugs()
-  return posts.map(({ slug }) => ({ slug: slug ?? '' }))
-}
+// export async function generateStaticParams() {
+//   const posts = await getPostsSlugs()
+//   return posts.map(({ slug }) => ({ slug: slug }))
+// }
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
