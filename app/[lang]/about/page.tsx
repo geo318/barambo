3dfa6d8488a69fe048b6d6fde39270599cb40d82
geo318 <geo.lomidze@gmail.com>
2023-11-26 03:09:48 +0400

    style: about mobile page

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -7,12 +7,12 @@ import { PageProps } from '/types'
 export default async function About({ params: { lang } }: PageProps) {
   const { about, home } = await getDictionary(lang)
   return (
-    <main className='flex flex-col gap-36'>
-      <Section className='bg-gold-light py-28'>
+    <main className='flex flex-col lg:gap-36 gap-10 text-justify mb-10'>
+      <Section className='bg-gold-light lg:py-28 py-10'>
         <H tag='h1' size='xl'>
           {about.h1}
         </H>
-        <div className='flex gap-[7%]'>
+        <div className='flex gap-[7%] lg:flex-row flex-col'>
           <div className='shrink-1'>
             <h2 className='font-medium text-2xl pb-4 border-b border-secondary table mb-4 mt-8'>
               {about.sub}
@@ -23,7 +23,7 @@ export default async function About({ params: { lang } }: PageProps) {
               ))}
             </div>
           </div>
-          <div className='basis-1/3 flex shrink-0'>
+          <div className='basis-1/3 flex shrink-0 lg:mt-0 mt-4'>
             <div>
               <Image
                 src={chocolate_about}
@@ -33,17 +33,17 @@ export default async function About({ params: { lang } }: PageProps) {
             </div>
           </div>
         </div>
-        <p className='mt-12 font-medium text-2xl'>{about.sum}</p>
+        <p className='mt-12 font-medium lg:text-2xl text-md'>{about.sum}</p>
       </Section>
       <Section>
-        <H tag='h2' className='mb-16' size='md'>
+        <H tag='h2' className='lg:mb-16 mb-4' size='md'>
           {about.mission.h2}
         </H>
-        <div className='flex gap-[7%]'>
-          <div className='basis-1/3 shrink-0'>
+        <div className='flex gap-[7%] lg:flex-row flex-col'>
+          <div className='basis-1/3 shrink-0 lg:mb-0 mb-4'>
             <Image src={ice_cream_about} alt='' className='sticky top-10' />
           </div>
-          <div className='flex flex-col gap-10 font-normal text-lg text-secondary'>
+          <div className='flex flex-col lg:gap-10 gap-4 font-normal lg:text-lg text-md text-secondary'>
             {about.mission.p.map((p, index) => (
               <p key={index}>{p}</p>
             ))}
