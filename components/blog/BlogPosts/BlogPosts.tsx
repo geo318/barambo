e1653dd21f251c806df676ff76b016d537d5fdd7
geo318 <geo.lomidze@gmail.com>
2023-11-27 14:17:08 +0400

    feat: add links to blogpost captions

--- components/blog/BlogPosts/BlogPosts.tsx
+++ components/blog/BlogPosts/BlogPosts.tsx
@@ -19,6 +19,14 @@ export function BlogPosts({
         : posts?.map((p) => (
             <div key={p.id}>
               <div className='flex aspect-square rounded-3xl relative overflow-hidden'>
+                <Link
+                  className='absolute inset-0 z-10'
+                  href={
+                    filter === 'recept'
+                      ? ''
+                      : `/${lang}/blog/${p.slug}?filter=${filter}`
+                  }
+                />
                 {p.thumbnail && (
                   <Image
                     src={getImage`${p.thumbnail}`}
@@ -28,23 +36,24 @@ export function BlogPosts({
                     className='absolute inset-0 object-cover h-full w-full'
                   />
                 )}
-                <Link
-                  href={
-                    filter === 'recept'
-                      ? `?filter=recept&recept=${p.slug}`
-                      : `/${lang}/blog/${p.slug}?filter=${filter}`
-                  }
-                  className='mt-auto mx-auto lg:mb-8 mb-4 z-10'
-                >
-                  <Button className='bg-white lg:w-36 lg:h-10 w-32 h-8 lg:text-xl text-md'>
-                    Read More
-                  </Button>
-                </Link>
+                {filter === 'recept' && (
+                  <Link
+                    href={`?filter=recept&recept=${p.slug}`}
+                    className='mt-auto mx-auto lg:mb-8 mb-4 z-10'
+                  >
+                    <Button className='bg-white lg:w-36 lg:h-10 w-32 h-8 lg:text-xl text-md'>
+                      Read More
+                    </Button>
+                  </Link>
+                )}
               </div>
               {filter !== 'recept' && (
-                <span className='py-2 lg:block hidden line-clamp-2 text-ellipsis'>
+                <Link
+                  href={`/${lang}/blog/${p.slug}?filter=${filter}`}
+                  className='py-2 lg:block hidden line-clamp-2 text-ellipsis'
+                >
                   {p[`title_${getLangKey(lang)}`]}
-                </span>
+                </Link>
               )}
             </div>
           ))}
