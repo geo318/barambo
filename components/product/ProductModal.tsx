7a88216eb237d04ad0383880e62c4f90a9e5aaff
geo318 <geo.lomidze@gmail.com>
2023-11-27 13:06:39 +0400

    feat: mobile product modal

--- components/product/ProductModal.tsx
+++ components/product/ProductModal.tsx
@@ -17,16 +17,16 @@ export async function ProductModal({
   return product ? (
     <Portal>
       <div className='xl:mx-48 lg:mx-28 mx-10 max-w-[110rem] mt-20'>
-        <div className='z-50 block p-9 relative bg-white rounded-[3rem]'>
+        <div className='z-50 block lg:p-9 p-3 relative bg-white lg:rounded-[3rem] rounded-xl'>
           <div className='flex'>
-            <H tag='h3' size='lg' className='mt-5'>
+            <H tag='h3' size='lg' className='lg:mt-5'>
               {product[`title_${getLangKey(lang)}`]}
             </H>
             <CloseModal closeKey='/product' />
           </div>
 
-          <section className='grid grid-cols-8 gap-12'>
-            <figure className='col-span-3'>
+          <section className='grid grid-cols-8 lg:gap-12'>
+            <figure className='lg:col-span-3 col-span-8'>
               <Image
                 src={getImage`${product.thumbnail}`}
                 alt='product'
@@ -36,8 +36,8 @@ export async function ProductModal({
               />
             </figure>
 
-            <div className='col-span-4 font-lg'>
-              <div className='my-10 border-b border-[#bebebe]' />
+            <div className='lg:col-span-4 col-span-8 font-lg'>
+              <div className='lg:my-10 my-4 border-b border-[#bebebe]' />
               <div
                 className='leading-loose text-secondary'
                 dangerouslySetInnerHTML={{
