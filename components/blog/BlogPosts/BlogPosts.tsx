0e7e8abc4c4a4706c60514bbd8bcd06df481d5f7
geo318 <geo.lomidze@gmail.com>
2023-11-30 13:56:34 +0400

    feat: increase blog img sizes

--- components/blog/BlogPosts/BlogPosts.tsx
+++ components/blog/BlogPosts/BlogPosts.tsx
@@ -33,8 +33,8 @@ export function BlogPosts({
                   <Image
                     src={getImage`${p.thumbnail}`}
                     alt={`${p.id}`}
-                    width={200}
-                    height={200}
+                    width={400}
+                    height={400}
                     className='absolute inset-0 object-cover h-full w-full'
                   />
                 )}
