04df04eea0f60f6a59358e4b2718511c3475ae08
geo318 <geo.lomidze@gmail.com>
2023-12-07 15:49:48 +0400

    chore: change social links

--- components/shared/Footer/Social.tsx
+++ components/shared/Footer/Social.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link'
-import { Fb, In, Yt, Tw } from '/components'
+import { Fb, In, Yt } from '/components'
 
 export const Social = () => (
   <div className='flex gap-3 lg:mr-auto lg:ml-0 ml-auto'>
-    <Link href='https://www.facebook.com/barambo' target='_blank'>
+    <Link href='https://www.facebook.com/barambo.ge' target='_blank'>
       <Fb className='lg:w-full lg:h-full w-8 h-8' />
     </Link>
-    <Link href='https://www.instagram.com/barambo' target='_blank'>
+    {/* <Link href='https://www.instagram.com/barmchocolate' target='_blank'>
       <Tw className='lg:w-full lg:h-full w-8 h-8' />
-    </Link>
-    <Link href='https://t.me/barambo' target='_blank'>
+    </Link> */}
+    <Link href='https://www.youtube.com/@BaramboChannel' target='_blank'>
       <Yt className='lg:w-full lg:h-full w-8 h-8' />
     </Link>
-    <Link href='https://www.linkedin.com/company/barambo' target='_blank'>
+    <Link href='https://www.linkedin.com/barmchocolate' target='_blank'>
       <In className='lg:w-full lg:h-full w-8 h-8' />
     </Link>
   </div>
