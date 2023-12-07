28dc62a39716312b70bc4e072a27447db37a36e8
geo318 <geo.lomidze@gmail.com>
2023-12-07 16:15:26 +0400

    style: product modal touchup

--- components/product/ProductModal.tsx
+++ components/product/ProductModal.tsx
@@ -14,16 +14,16 @@ export async function ProductModal() {
 
   return product ? (
     <Portal>
-      <div className='xl:mx-48 lg:mx-28 mx-10 max-w-[110rem] mt-20'>
-        <div className='z-50 block lg:p-9 p-3 relative bg-white lg:rounded-[3rem] rounded-xl'>
+      <div>
+        <div className='z-50 block lg:p-9 p-3 relative bg-white lg:rounded-[3rem] rounded-xl xl:mx-48 lg:mx-28 max-h-[80vh] overflow-y-auto mx-10 max-w-[110rem] mt-20'>
           <div className='flex'>
-            <H tag='h3' size='lg' className='lg:mt-5'>
+            <H tag='h3' size='lg' className='lg:mt-5 !text-lg'>
               {product[`title_${getLangKey(lang)}`]}
             </H>
             <CloseModal closeKey={`/${lang}/product`} />
           </div>
 
-          <section className='grid grid-cols-8 lg:gap-12'>
+          <section className='lg:grid grid-cols-8 lg:gap-12'>
             <figure className='lg:col-span-3 col-span-8'>
               <Image
                 src={getImage`${product.thumbnail}`}
