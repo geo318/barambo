504e1a17e6bda8b8d8fdb8d677525468f4e64032
geo318 <geo.lomidze@gmail.com>
2023-12-12 17:26:37 +0400

    feat: persist searchparams when language is switched

--- components/shared/switcher.tsx
+++ components/shared/switcher.tsx
@@ -2,11 +2,18 @@
 
 import Link from 'next/link'
 import { localeInfo } from '/config'
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 import { twMerge } from 'tailwind-merge'
 
 export function Switcher({ className = '' }) {
   const pathname = usePathname()
+  const params = useSearchParams()
+  let searchParams = '?'
+
+  for (const [key, value] of params.entries()) {
+    searchParams += `${key}=${value}&`
+  }
+
   return (
     <ul className={twMerge('font-medium text-lg gap-4', className)}>
       {localeInfo?.map((locale) => (
@@ -19,7 +26,9 @@ export function Switcher({ className = '' }) {
           )}
         >
           <Link
-            href={pathname.replace(/^\/(en|ka)/, `/${locale.key}`)}
+            href={
+              pathname.replace(/^\/(en|ka)/, `/${locale.key}`) + searchParams
+            }
             className='hover:opacity-80 transition-opacity duration-200 uppercase'
           >
             {locale.name}
